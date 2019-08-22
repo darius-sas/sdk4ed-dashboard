148253a491f9a0cf1c6e1470c5c984c27c7f3e68
Darius Sas <darius.sas@outlook.com>
2019-08-22 15:56:13 +0200

    Added basic file explorer

--- src/components/sideNavigation.js
+++ src/components/sideNavigation.js
@@ -1,70 +1,21 @@
-import React, {Fragment, PureComponent} from 'react';
+import React, {PureComponent} from 'react';
 import logo from "../assets/logo.png"
-import {Treebeard} from 'react-treebeard';
-
-const data = {
-    name: 'root',
-    toggled: true,
-    children: [
-        {
-            name: 'parent',
-            children: [
-                { name: 'child1' },
-                { name: 'child2' }
-            ]
-        },
-        {
-            name: 'loading parent',
-            loading: true,
-            children: []
-        },
-        {
-            name: 'parent',
-            children: [
-                {
-                    name: 'nested parent',
-                    children: [
-                        { name: 'nested child 1' },
-                        { name: 'nested child 2' }
-                    ]
-                }
-            ]
-        }
-    ]
-};
+import FileExplorer from "./pages/sections/FileExplorer"
 
 
 class TopNavigation extends PureComponent {
-    constructor(props){
-        super(props);
-        this.state = {data};
-    }
-    
-    onToggle(node, toggled){
-        const {cursor, data} = this.state;
-        if (cursor) {
-            this.setState(() => ({cursor, active: false}));
-        }
-        node.active = true;
-        if (node.children) { 
-            node.toggled = toggled; 
-        }
-        this.setState(() => ({cursor: node, data: Object.assign({}, data)}));
-    }
-    
+   
     render(){
-        const {data} = this.state;
         return (
+            <React.Fragment>
             <div className="sidebar-fixed position-fixed">
             <a href="#!" className="logo-wrapper waves-effect">
                 <img alt="SDK4ED logo" className="img-fluid" src={logo}/>
             </a>
-            <div><span>Project explore tree here</span></div>
-            <Treebeard
-                data={data}
-                onToggle={this.onToggle}
-            />
+            <div><span>MyProject</span></div>
+            <FileExplorer/>
             </div>
+            </React.Fragment>
         );
     }
 }
