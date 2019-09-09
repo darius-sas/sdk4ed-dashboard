97515233b8291d9fe79da5630b9a988d81028dea
Angeliki Tsintzira <angeliki.agathi.tsintzira@gmail.com>
2019-09-09 08:44:17 +0000

    Minor changes in TD New Code.

--- src/components/pages/TDNewCodeDashPage.js
+++ src/components/pages/TDNewCodeDashPage.js
@@ -62,11 +62,11 @@ const NewCodePanel = props =>{
    
     series: [{
         name: 'TD Density New Code',
-        data: props.densityNC.data,
+        data: props.densityTD.data,
         color: "#278649",
     }, {
-        name: 'TD Density',
-        data: props.densityTD.data,
+        name: 'TD Density Project',
+        data: props.densityNC.data,
         color: "#8E2E26",
     }]
     };
