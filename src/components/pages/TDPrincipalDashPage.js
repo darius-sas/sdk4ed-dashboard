1e64e804145a35b6bdda73f52f97b423a11f1ddd
Darius Sas <darius.sas@outlook.com>
2019-11-11 15:18:28 +0100

    Restored working version of TDPrincipal page

--- src/components/pages/TDPrincipalDashPage.js
+++ src/components/pages/TDPrincipalDashPage.js
@@ -29,26 +29,7 @@ const radarChartOptions = {
     legend: false,
 }
 
-
-
-
-const PrincipalPanel = props => {
-		
-	function normalize(min, max) 
-	{
-		var delta = max - min;
-		return function (val) {
-			return (val - min) / delta;
-		};
-	}
-
-	var numbers = [parseFloat(props.principal.bugs), parseFloat(props.principal.vulnerabilities), parseFloat(props.principal.duplCode), parseFloat(props.principal.codeSmells)];
-
-	console.log(numbers.map(normalize(Math.min(...numbers), Math.max(...numbers))));
-
-	numbers = numbers.map(normalize(Math.min(...numbers), Math.max(...numbers)))
-
-	const PrincipalRadarPanel =  {
+const InterestRadarPanel =  {
         labels: ['Bugs', 'Vulnerabilities', 'Duplicated Lines  Density', 'Code Smells'], 
         datasets: [
             {
@@ -61,89 +42,13 @@ const PrincipalPanel = props => {
                 pointBorderColor: '#c1c7d1',
                 pointHoverBackgroundColor : '#fff',
                 pointHoverBorderColor: 'rgba(84,130,53,1)',
-                data: numbers
+                data:[0.9805476228578603, 1.0, 0.7897392767031118, 0.28] // data values from prop var are loaded here
             }
         ]
     }
-<<<<<<< HEAD
 
 
 const PrincipalPanel = props => {    
-=======
-	
-	
-   //============== Example code ==============//
-    var seriesData = [];
-    const options = {
-        chart: {
-        polar: true,
-        type: 'line'
-    },
-
-    title: {
-        text: 'Principal over time',
-        x: -80
-    },
-
-    pane: {
-        size: '80%'
-    },
-
-    xAxis: {
-        categories: ['Code Smells', 'Bugs', 'Vulnerabilities', 'Duplicated Lines Density'],
-        tickmarkPlacement: 'on',
-        lineWidth: 0
-    },
-
-    yAxis: {
-        gridLineInterpolation: 'polygon',
-        lineWidth: 0,
-        min: 0
-    },
-
-    tooltip: {
-        shared: true,
-        pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
-    },
-
-    legend: {
-        align: 'right',
-        verticalAlign: 'middle'
-    },
-
-    series: [{
-        name: 'Artifact Value',
-        data: [500, 190, 600, 350],
-        pointPlacement: 'on'
-    }, {
-        name: 'Average Value',
-        data: [230, 243, 231, 242],
-        pointPlacement: 'on'
-    }],
-
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                legend: {
-                    align: 'center',
-                    verticalAlign: 'bottom'
-                },
-                pane: {
-                    size: '70%'
-                }
-            }
-        }]
-    }
-
-    }
-    
-    
-    //=========================================//
-    
->>>>>>> d67f580112d5d5093e7903800c3796a72feaba24
   return (
    <PagePanel header="Technical debt principal" linkTo="principal">            
    <MDBRow className="mb-4">
@@ -157,17 +62,11 @@ const PrincipalPanel = props => {
                                 Select project
                             </MDBDropdownToggle>
                             <MDBDropdownMenu basic>
-<<<<<<< HEAD
                             {props.projects.map((element, index) => (
                                 <MDBDropdownItem key={index} onClick={() => props.fetchData(element)}>
                                 {element}
                                 </MDBDropdownItem>
                             ))}
-=======
-                                <MDBDropdownItem onClick={(param) => props.updateProjectData('holisun_arassistance')}>Holisun Arassistance</MDBDropdownItem>
-                                <MDBDropdownItem onClick={(param) => props.updateProjectData('airbus')}>Airbus</MDBDropdownItem>
-                                <MDBDropdownItem onClick={(param) => props.updateProjectData('neurasmus')}>Neurasmus</MDBDropdownItem>
->>>>>>> d67f580112d5d5093e7903800c3796a72feaba24
                             </MDBDropdownMenu>
                         </MDBDropdown>
                         <h4 className="ml-3" style={{color:'#548235'}}>{props.currentProjectName}</h4>
@@ -182,7 +81,7 @@ const PrincipalPanel = props => {
        <MDBCol size="12">
                 <MDBRow className="mb-3">
                   <MDBCol>
-                  <CountCard title="TD IN MINUTES"  value={props.principal.tdInMinutes} icon="clock" />
+                  <CountCard title="TD IN DAYS"  value={props.principal.tdInDays} icon="clock" />
                   </MDBCol>
                   <MDBCol>
                   <CountCard title="TD IN CURRENCY" color="#33691e light-green darken-4" value={props.principal.tdInCurrency} icon="money-bill-alt"/>
@@ -190,13 +89,16 @@ const PrincipalPanel = props => {
                   <MDBCol>
                   <CountCard title="BUGS" color="#33691e light-green darken-4" value={props.principal.bugs} icon="bug"/>
                   </MDBCol>
-                   </MDBRow>
-                  <MDBRow className="mb-3">
                   <MDBCol>
                   <CountCard title="VULNERABILITIES" color="#33691e light-green darken-4" value={props.principal.vulnerabilities} icon="lock-open"/>
                   </MDBCol>
+                   </MDBRow>
+                  <MDBRow className="mb-3">
 				  <MDBCol>
                   <CountCard title="CODE SMELLS" color="#33691e light-green darken-4" value={props.principal.codeSmells} icon="compress-arrows-alt"/>
+                  </MDBCol>
+                   <MDBCol>
+                  <CountCard title="COVERAGE (%)" color="#33691e light-green darken-4" value={props.principal.coverage} icon="fire-alt"/>
                   </MDBCol>
                    <MDBCol>
                   <CountCard title="DUPLICATIONS (%)" color="#33691e light-green darken-4" value={props.principal.duplCode} icon="copy"/>
@@ -207,7 +109,6 @@ const PrincipalPanel = props => {
       
        <MDBRow className="mb-3">
   
-<<<<<<< HEAD
       <MDBCol md="12" lg="12" className="mb-12">
                     <MDBCard className="mb-12">
                     <MDBCardHeader className="sdk4ed-color">Principal Indicators</MDBCardHeader>
@@ -224,48 +125,6 @@ const PrincipalPanel = props => {
       </PagePanel>
 )}
 
-=======
-   <MDBCol md="12" lg="12" className="mb-12">
-                <MDBCard className="mb-12">
-                <MDBCardHeader className="sdk4ed-color">Principal Indicators</MDBCardHeader>
-                <MDBCardBody>
-                    <MDBContainer>
-                        <Radar data={PrincipalRadarPanel} options={radarChartOptions} />
-                    </MDBContainer>
-                </MDBCardBody>
-                </MDBCard>
-            </MDBCol> 
-   </MDBRow>
-   
-          <MDBCol md="12">
-            <BasicTable title="Principal Indicators" data={props.principalArtifacts}/>
-          </MDBCol>
-          
-        
-      </PagePanel>
-)}
-
-
-
-const FileExplorerPanel = () => {
-  return (
-    <ContentPanel title="Project explorer">
-      <FileExplorer></FileExplorer>
-    </ContentPanel>
-  )
-}
-
-
-// Function to extract values from json
-function returnValues(data) {
-    var values = []
-    for(var i = 0; i < data.length; i++) {
-        values.push(data[i].eval)
-    }
-    return values
-}
-
->>>>>>> d67f580112d5d5093e7903800c3796a72feaba24
 /**
  * The technical debt dashboard page. The page is assembled using multiple panels.
  * The data is retrieved asynchronously.
@@ -280,64 +139,13 @@ class TDPrincipalDashPage extends React.Component {
     this.updateOnSelect = this.updateOnSelect.bind(this)
 
     this.state = {
-<<<<<<< HEAD
       principalSummary: null, // Principal-related summary information
       principalIndicatorsRadar: null,
       projectNodes: null, // File structure of the project
       rawData: null, // Raw data as received from the server for a given project.
       currentProjectName: "", // Current project full name
-=======
-		isLoading: false,
-		name: '',
-		principalIndicatorsSummary: {},
-		principalIndicators: {},
->>>>>>> d67f580112d5d5093e7903800c3796a72feaba24
     }
   }
-  
-	// Update project 
-	updateProjectData = (projectName) => {
-		this.setState({ 
-            isLoading: true,
-        });
-		
-		if(projectName === 'neurasmus'){
-			fetch("http://127.0.0.1:3001")
-			.then(resp => resp.json())
-			.then(resp => {
-				this.setState({
-					isLoading: false,
-					name: resp.neurasmusTD.projectName,
-					principalIndicatorsSummary: resp.neurasmusTD.principalSummary,
-					principalIndicators: resp.neurasmusTD.principalIndicators,
-					principalLineChart: resp.neurasmusTD.lineChartTD,
-				})
-			})
-		}else if(projectName === 'holisun_arassistance'){
-			fetch("http://127.0.0.1:3001")
-			.then(resp => resp.json())
-			.then(resp => {
-				this.setState({
-					isLoading: false,
-					name: resp.holisun_arassistanceTD.projectName,
-					principalIndicatorsSummary: resp.holisun_arassistanceTD.principalSummary,
-					principalIndicators: resp.holisun_arassistanceTD.principalIndicators,
-				})
-			})
-		}else if(projectName === 'airbus'){
-			fetch("http://127.0.0.1:3001")
-			.then(resp => resp.json())
-			.then(resp => {
-				this.setState({
-					isLoading: false,
-					name: resp.airbusTD.projectName,
-					principalIndicatorsSummary: resp.airbusTD.principalSummary,
-					principalIndicators: resp.airbusTD.principalIndicators,
-				})
-			})
-		}
-	}
-  
 
   getApiURL(){
     const host = "http://se.uom.gr:9906/api/"
@@ -345,7 +153,6 @@ class TDPrincipalDashPage extends React.Component {
   }
 
   componentDidMount(){
-<<<<<<< HEAD
     this.fetchProjectData("neurasmus8")
   }
 
@@ -372,32 +179,11 @@ class TDPrincipalDashPage extends React.Component {
 
   render(){
     if(this.state.principalSummary == null){
-=======
-    fetch("http://127.0.0.1:3001")
-    .then(resp => resp.json())
-    .then(resp => {
-      console.log("Data received")
-      this.setState({
-				isLoading: false,
-				name: resp.holisun_arassistanceTD.projectName,
-				principalIndicatorsSummary: resp.holisun_arassistanceTD.principalSummary,
-				principalIndicators: resp.holisun_arassistanceTD.principalIndicators,
-			})
-    })
-  }
-
-  render(){
-	  		const { isLoading, name, principalIndicatorsSummary, principalIndicators } = this.state
-
-	  
-    if(this.isLoading){
->>>>>>> d67f580112d5d5093e7903800c3796a72feaba24
       return (<Loader/>)
     }else{
       return(
           <React.Fragment>
             <MDBRow>
-<<<<<<< HEAD
               <MDBCol size="3">
               <ContentPanel title="Project explorer">
                   <FileExplorer onFetchData={() => {return this.state.projectNodes} }
@@ -410,20 +196,6 @@ class TDPrincipalDashPage extends React.Component {
                               projects={projects}
                               fetchData={this.fetchProjectData}
                               currentProjectName={this.state.currentProjectName}/>
-=======
-            {/*
-              <MDBCol size="2">
-              <FileExplorerPanel/>
-              </MDBCol>*/}
-              <MDBCol>
-              <PrincipalPanel 
-					   myprojectName = {name}
-                       updateProjectData={this.updateProjectData} 
-                       principal = {principalIndicatorsSummary}
-                       principalArtifacts = {principalIndicators}
-               />
-              
->>>>>>> d67f580112d5d5093e7903800c3796a72feaba24
               </MDBCol>
               </MDBRow>
             </React.Fragment>)
